b949b964a28c4feadf8a81f854261979bf3a9b23
ronouk <shubro42@yahoo.com>
2024-07-01 12:43:38 +0600

    mongo connected

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require("express")
-const cors = require ("cors")
+const mongoose = require("mongoose")
+const cors = require("cors")
 const app = express();
 const port = process.env.PORT || 5000;
+
+// art-craft-db
+// azHvm7ycBZDNkL9X
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get( "/", (req, res) =>{
+app.get("/", (req, res) => {
     res.send("Art and craft server is running")
 })
 
-app.listen(port, () =>{
-    console.log("Coffee server is running on port: ", port)
-})
+mongoose.connect("mongodb+srv://art-craft-db:azHvm7ycBZDNkL9X@cluster0.jjpytgu.mongodb.net/art-craft-database?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        console.log("Connected to the mongo database")
+
+        //start the application
+        app.listen(port, () => {
+            console.log("Coffee server is running on port: ", port)
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
